69252abe0ff730bd01ad4df31a402921c325ae96
Jiuse3 <114151083+Jiuse3@users.noreply.github.com>
2022-10-10 16:06:11 +0100

    Add files via upload

---
+++ naming.js
@@ -0,0 +1,31 @@
+var pet_preferito = 'gatto';
+console.log(pet_preferito);
+
+var pet = 'gatto';
+var Pet = 'cane';
+var petPreferito = 'criceto';
+var petPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+//var _pet2 = 'pantera';
+//document.write(_pet2);
+
+var dodici = (12);
+
+var gol_fatti=12;
+var gol_subiti=10;
+
+console.log (gol_fatti-gol_subiti);
+
+var valore = 11;
+//console.log(valore);
+
+console.log(valore-7);
+
+document .getElementById('luca').innerHTML='ciaobello'
+
+volte=11
+
